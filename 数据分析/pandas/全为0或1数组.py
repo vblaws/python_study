cecc82db858351ffb2dac1a20558238ad57dee3a
vblaws <H3239498031@gmail.com>
2024-01-17 08:51:19 +0800

    Python代码

--- 数据分析/pandas/全为0或1数组.py
+++ 数据分析/pandas/全为0或1数组.py
@@ -9,9 +9,10 @@ df = pd.read_csv(file_path)
 # Genre表示类型的意思
 temp_list = df["Genre"].str.split(",").tolist()  # [[],[],[]]
 
+print(df["Genre"].head(3))
+
 genre_data = list(set([i for j in temp_list for i in j]))  # 先转换为集合去重,然后转换为列表
 print(type(genre_data))
-# print(len(set(genre_data)))
 
 # 构造全为0的数组,列数为每一个种类
 zero_list = pd.DataFrame(np.zeros((df.shape[0], len(genre_data))), columns=genre_data)
