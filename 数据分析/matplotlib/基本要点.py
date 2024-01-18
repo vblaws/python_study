17bfdaabb9906bc9fa4ad6831795c40b5bc457cf
vblaws <H3239498031@gmail.com>
2024-01-18 13:20:06 +0800

    这是我的python代码

--- 数据分析/matplotlib/基本要点.py
+++ 数据分析/matplotlib/基本要点.py
@@ -2,12 +2,15 @@ from matplotlib import pyplot as plt
 
 # 可以理解为画布
 plt.figure(figsize=(20, 8), dpi=100)
+
 x = range(2, 26, 2)
+
 y = [15, 13, 12, 21, 43, 32, 43, 23, 23, 43, 43, 12]
+# 画折线图的方法
 plt.plot(x, y)
 # 设置x轴刻度
-_x_lab = [i / 2 for i in range(4, 49)]
-plt.xticks(_x_lab[::3])
-plt.yticks(range(min(y), max(y)))
+# _x_lab = [i / 2 for i in range(4, 49)]
+# plt.xticks(_x_lab[::3])
+# plt.yticks(range(min(y), max(y)))
 plt.savefig("D:\\matplotlib可视化图\\我的第一张matplotlib图.png")
 plt.show()
